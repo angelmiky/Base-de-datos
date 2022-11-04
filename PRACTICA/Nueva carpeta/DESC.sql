3a0d368fb6a99052e2cf8fcc1e9e9d705c8b9e6a
angelmiky708@gmail.com <daw2221@CALASANZSALAMANCA.local>
2022-11-04 10:12:15 +0100

    bbdd

---
+++ PRACTICA/Nueva carpeta/DESC.sql
@@ -0,0 +1,1 @@
+SELECT * from country where LifeExpectancy>70
---
+++ PRACTICA/Nueva carpeta/Final.sql
@@ -0,0 +1,6 @@
+SELECT * FROM world.country;
+update country set avgpopulation=Population/SurfaceArea; 
+insert into country values ('ESP', 'Spain', 'Europe', 'Southern Europe', '505992.00', 
+'1492', '39441700', '77.95', '78.8', '553233.00', '532031.00', 'España', 'Constitutional 
+Monarchy', 'Juan Carlos I', '653', 'ES', 'Teide');
+
